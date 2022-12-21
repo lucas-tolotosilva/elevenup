e4161ef128bc8365a07d7ff46c4611c32a3b25ff
unknown <lucastoloto.silva@gmail.com>
2022-12-21 09:47:55 -0300

    Setting the Project's dependencies

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <img className="w-full h-full rounded-full" src="https://i.pinimg.com/550x/d4/1d/b5/d41db56632ba67ff476b0fd98f1fed89.jpg"/>
+    </div>
+  )
+}
+
+export default App
