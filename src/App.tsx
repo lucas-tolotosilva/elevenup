88be273597ea3219adbd0b378ec119e56eb5c7e1
unknown <lucastoloto.silva@gmail.com>
2022-12-22 11:30:53 -0300

    Setting Fonts and Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './styles/main.css'
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App bg-gray-200">
       <img className="w-full h-full rounded-full" src="https://i.pinimg.com/550x/d4/1d/b5/d41db56632ba67ff476b0fd98f1fed89.jpg"/>
+      <h1>oajpoajpodjdposdjsopdjoasjpsodjposajposjapojasopdj</h1>
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,43 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Bebas Neue, Poppins, sans-serif'
+      },
+      colors: {
+        blue: {
+          600: '#025ead',
+          800: '#003868'
+        }, 
+        pink: {
+          800: '#610950',
+          500: '#ff0ed1'
+        },
+        black: {
+          900: '#222222'
+        },
+        gray: {
+          200: '#ececec'
+        } 
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      }
+    },
   },
   plugins: [],
 }
