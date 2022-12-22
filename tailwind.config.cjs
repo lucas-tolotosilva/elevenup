61029d2948a1ac20c156b23d6ca302f45fba6e76
unknown <lucastoloto.silva@gmail.com>
2022-12-22 11:33:37 -0300

    Add Screens Tailwind Configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -26,20 +26,14 @@ module.exports = {
         } 
       },
       screens: {
-        'sm': '640px',
+        'sm': '320px',
         // => @media (min-width: 640px) { ... }
   
-        'md': '768px',
+        'md': '760px',
         // => @media (min-width: 768px) { ... }
   
         'lg': '1024px',
         // => @media (min-width: 1024px) { ... }
-  
-        'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-  
-        '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
       }
     },
   },
