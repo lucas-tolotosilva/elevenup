722ca9d6aaa19e8099bb79e372753e22cbb6e0f0
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2022-12-26 11:44:33 -0300

    Adding image circles in the Compromissos Section

--- src/components/Compromisso.tsx
+++ src/components/Compromisso.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import c1 from '../assets/circles/circulo-1.png'
+import c2 from '../assets/circles/circulo-2.png'
+import c3 from '../assets/circles/circulo-3.png'
+import c4 from '../assets/circles/circulo-4.png'
 
 function Compromisso () {
     return (
@@ -16,19 +20,19 @@ function Compromisso () {
 
             <div className="w-6/12 grid grid-cols-2 gap-12 px-14">
                 <div className='bg-blue-900'>
-                    <img/>
+                    <img src={ c1 }/>
                     <span></span>
                 </div>
                 <div className='bg-blue-900'>
-                    <img />
+                    <img src={ c2 } />
                     <span></span>
                 </div>
                 <div className='bg-blue-900'>
-                    <img />
+                    <img src={ c3 } />
                     <span></span>
                 </div>
                 <div className='bg-blue-900'>
-                    <img />
+                    <img src={ c3 } />
                     <span></span>
                 </div>
             </div>
