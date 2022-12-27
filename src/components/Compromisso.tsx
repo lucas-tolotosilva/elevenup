82b474a76a5591cfa6622cc406dc9af8c3dd74c6
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2022-12-27 10:57:30 -0300

    change2

--- src/components/Compromisso.tsx
+++ src/components/Compromisso.tsx
@@ -16,7 +16,7 @@ function Compromisso () {
                             mercado,    além     de    Biomédicos    e   Biólogos
                             com   mestrado  e  doutorado  e  Engenheiros   de
                             Bioprocessos, formados    nas   mais   renomadas
-                            universidades do país..
+                            universidades do país.
                         </p>
                     {/* </div> */}
                 </div>
