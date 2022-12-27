2617d58b55bd029dcd077010357a0da5a0180bcb
joeyanovich <joedison.dias@gmail.com>
2022-12-27 10:36:07 -0300

    Trying new things

--- src/components/Compromisso.tsx
+++ src/components/Compromisso.tsx
@@ -7,10 +7,10 @@ import c4 from '../assets/circles/circulo-4.png'
 function Compromisso () {
     return (
         <div className='bg-white-200 my-72 w-9/12 rounded-3xl border-1 border-white-100 flex items-center py-20'>
-            <div className="w-6/12 m-0 flex flex-col justify-center gap-8 ">
-                <div>
+            <div className="w-6/12 m-0 flex flex-col justify-center gap-8">
+                <div className="max-w-[568px] bg-cyan-500">
                     {/* <div className='m-0 w-3/4  bg-cyan-300'> */}
-                        <h1 className='max-w-[568px] font-title w-full text-9xl px-14'>Compromissos</h1>
+                        <h1 className='font-title w-full text-9xl px-14'>Compromissos</h1>
                         <p className='font-default text-2xl text-justify px-14'>Nosso   time   contém   especialistas   em  design,
                             tráfego em   canais   digitais   e    inteligência   de
                             mercado,    além     de    Biomédicos    e   Biólogos
