777c8724bd405d461b594a15aec7f1d59353ffbb
joeyanovich <joedison.dias@gmail.com>
2022-12-27 10:45:37 -0300

    Testing things

--- src/components/Compromisso.tsx
+++ src/components/Compromisso.tsx
@@ -10,13 +10,13 @@ function Compromisso () {
             <div className="w-6/12 m-0 flex flex-col justify-center gap-8">
                 <div className="max-w-[568px] bg-cyan-500">
                     {/* <div className='m-0 w-3/4  bg-cyan-300'> */}
-                        <h1 className=' font-title w-full text-9xl px-14'>Compromissos</h1>
+                        <h1 className='font-title w-full text-9xl px-14'>Compromissos</h1>
                         <p className='font-default text-2xl text-justify px-14'>Nosso   time   contém   especialistas   em  design,
                             tráfego em   canais   digitais   e    inteligência   de
                             mercado,    além     de    Biomédicos    e   Biólogos
                             com   mestrado  e  doutorado  e  Engenheiros   de
                             Bioprocessos, formados    nas   mais   renomadas
-                            universidades do país.
+                            universidades do país.....
                         </p>
                     {/* </div> */}
                 </div>
