33a24fed35d6eefff21751434dc5204bea8f55f2
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2022-12-30 12:14:51 -0300

    Add Locomotive Scroll

--- src/components/Clientes.tsx
+++ src/components/Clientes.tsx
@@ -1,30 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { data } from "./DataImg";
-import '../styles/style.css'
+import '../styles/locomotive-scroll.css'
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
 
 export function Clientes () {
-    const target = document.getElementById('client-logo')
-    const animationClass = 'animate';
+    // const [scroll, handleScroll] = useState(false);
+    // const [slide, handleSlide] = useState(52)
+    // var lastPosition = 0
+    
 
-    function animeScroll() {
-        console.log('1')
-    }
+    // const transitionScroll = () => {
+    //     var currentPosition = window.scrollY;
 
-    window.addEventListener('scroll', (event) => {
-        animeScroll;
-    })
+    //     // Cógido para 
+    //     if(currentPosition > 2100){
+    //         handleScroll(true);
+            
+    //         if(currentPosition > lastPosition){
+    //             handleSlide(slide + 4)
+                
+    //             console.log('BAIXO')
+    //         } else {
+    //             console.log('CIMA')
+    //         }
+            
+    //         lastPosition = currentPosition
+    //     } else {
+    //         handleScroll(false)
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     console.log(slide)
+    // }, [slide])
+    // useEffect(() => {
+    //     window.addEventListener("scroll", transitionScroll);
+    // }, [])
 
     return (
         <div className="w-full">
             <h2 className="font-title w-full text-[200px]">Clientes</h2>
+            
+            
+            <div data-scroll-container>
+            <div data-scroll-section>
+                <h1 data-scroll>Hey</h1>
+                <p data-scroll>👋</p>
+            </div>
+            <div data-scroll-section>
+                <h2 data-scroll data-scroll-speed="1">What's up?</h2>
+                <p data-scroll data-scroll-speed="2">😬</p>
+            </div>
+        </div>
+            
+            
             <div className="w-full bg-blue-400 mb-36"> 
-                <div data-anime="left" id="client-logo" className="flex">
+                <div data-anime="left" id="client-logo" className={`flex gap-12 mb-[2000px] `}>
                     {data.map((item)=> (
                         <img key={item.id} className="cover" src={item.img} alt='imagem' />
                     ))}
+
+                    
                 </div>
                 <div>
-                    ...
+                <div className="font-title text-6xl">    
+                       
+                    </div>
                 </div>
             </div>
         </div>
