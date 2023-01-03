34747f68bb410e3cf7c205e9e7a5a1a606322c20
unknown <lucastoloto.silva@gmail.com>
2023-01-03 09:10:14 -0300

    Update

--- src/components/Clientes.tsx
+++ src/components/Clientes.tsx
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { data } from "./DataImg";
 import '../styles/locomotive-scroll.css'
-import LocomotiveScroll from 'locomotive-scroll';
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-});
 
 export function Clientes () {
     // const [scroll, handleScroll] = useState(false);
@@ -45,30 +39,17 @@ export function Clientes () {
     return (
         <div className="w-full">
             <h2 className="font-title w-full text-[200px]">Clientes</h2>
-            
-            
-            <div data-scroll-container>
-            <div data-scroll-section>
-                <h1 data-scroll>Hey</h1>
-                <p data-scroll>👋</p>
-            </div>
-            <div data-scroll-section>
-                <h2 data-scroll data-scroll-speed="1">What's up?</h2>
-                <p data-scroll data-scroll-speed="2">😬</p>
-            </div>
-        </div>
-            
+                      
             
             <div className="w-full bg-blue-400 mb-36"> 
                 <div data-anime="left" id="client-logo" className={`flex gap-12 mb-[2000px] `}>
                     {data.map((item)=> (
                         <img key={item.id} className="cover" src={item.img} alt='imagem' />
                     ))}
-
-                    
                 </div>
+
                 <div>
-                <div className="font-title text-6xl">    
+                    <div className="font-title text-6xl">    
                        
                     </div>
                 </div>
