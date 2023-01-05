dd583817a95ed7b4b9fc981008511c7061c36d0e
unknown <lucastoloto.silva@gmail.com>
2023-01-05 12:01:56 -0300

    Add the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import bg from '../assets/fundo-footer.png'
+import bgBlur from '../assets/bg-blur.png'
 
 export function Footer () {
     return (
-        <div className=''>
+        <div className='w-full  relative'>
+            <div className='w-32 h-32 absolute z-10 bg-blue-500'>
 
+            </div>
+            <div className='relative'>
+                <div className='absolute w-full h-full'>
+                    <img className='cover w-full h-full' src={bgBlur}/>
+                </div>
+                <div className='absolute w-full h-full'>
+                    <img className=' cover w-full h-full' src={bg}/>
+                </div>
+            </div>
         </div>
     )
 }
