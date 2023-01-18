502d66fe8180b86f2dd54fcbadb01ea7cb209610
unknown <lucastoloto.silva@gmail.com>
2023-01-18 10:01:47 -0300

    Update Slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -6,14 +6,11 @@ import { Healthcare, Lifescience } from "./SegTextos";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 
 export function Slider () {
-    //https://www.youtube.com/results?search_query=react+3d+carousel
 
-    const posSlide1 = 'absolute z-10 left-0 hover:cursor-pointer w-[390px] h-[412px] bg-center bg-cover mt-10 ml-10'
-    const posSlide2 = 'absolute z-0 right-0 opacity-20'
 
     const variants = {
-        current : { x: 0, opacity: 1, scale: 1.25},
-        prev: {x: 200, opacity: 0.20, scale: 0.75}
+        current : { x: 0, opacity: 1, scale: 1.15},
+        prev: {x: 50, opacity: 0.20, scale: 0.75}
     }
 
     const slides = [
@@ -34,11 +31,6 @@ export function Slider () {
     const [currentIndex, setCurrentIndex] = useState(0);
     const [isToggled, setIsToggled] = useState(false);
 
-    // const handleToggled = () => {
-    //     setIsToggled(!isToggled)
-    // }
-
-
     const prevSlide = () => {
         const isFirstSlide = currentIndex === 0;
         const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
@@ -56,23 +48,18 @@ export function Slider () {
         
         setCurrentIndex(newIndex)
     }
-console.log(!isToggled)
+
+    const posSlide1 = `absolute z-10 left-0 w-[380px] h-[412px] bg-center bg-cover  mt-10 ml-10`
+    const posSlide2 = `absolute z-0 right-0 bg-${isToggled ?slides[0].color : slides[1].color} opacity-20 `
     return (
         <div className="w-[1440px] flex mb-36">
 
             <div className="w-6/12">
                 <div className="w-full h-full flex items-center mr-12 mb-36 relative">
-                    <div className=" w-full pl-14">
-                       
-                        {/* --------- Slide --------- */}
-                        <div className="flex">
-                            {/* <motion.div  style={{backgroundImage: `url(${slides[currentIndex].slide})`}} className="z-10 hover:cursor-pointer w-[390px] h-[412px] bg-center bg-cover mt-10 ml-10"
-                                initial={{ opacity:0 ,x: -300, scale: 0.5}}
-                                whileInView={{ opacity: 1 , x: 0, scale: 1}}
-                                transition={{ type: "spring", delay: 0.10 }}
-                                whileHover={{scale:1.15}}
-                                whileFocus={{x:100}}></motion.div>   */}
-                            <div className="flex">
+                    <div className="h-5/6 w-full pl-14">
+                       {/* --------- Slide --------- */}
+                       <div className="flex">
+                            <div className="flex ">
                                     {slides.map((slide, index) => {
                                         return <motion.img key={index} src={slide.slide} className={`${currentIndex === index ? posSlide1 : posSlide2}`}
                                                 animate={currentIndex === index ? "current" : "prev"}
@@ -82,8 +69,8 @@ console.log(!isToggled)
                                     })}
                             </div>
                             
-                            <div className="flex items-center justify-center relative">
-                                <motion.div className="flex items-center justify-center w-auto absolute bottom-0 left-12"
+                            <div className="flex bg-black-900 ">
+                                <motion.div className="flex items-center justify-center w-auto absolute bottom-1/2 right-16"
                                     initial={{ opacity:0 , scale: 0}}
                                     whileInView={{ opacity: 1 , scale: 1}}
                                     transition={{ type: 'spring',delay: 0.8, bounce: 0.55 }}>
@@ -99,6 +86,7 @@ console.log(!isToggled)
                             </div>
                         </div>
                         {/* --------- Fim Slide --------- */}
+                        
                     </div>
                 </div>
             </div>
