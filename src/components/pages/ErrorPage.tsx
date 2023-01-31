e62267bde700860afce28696f83cd98da0b39db7
unknown <lucastoloto.silva@gmail.com>
2023-01-31 10:55:18 -0300

    Updates

--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -5,15 +5,14 @@ export function ErrorPAge(){
     return (
         <div className="relative overflow-hidden w-screen h-screen flex flex-col items-center justify-center text-center">
             
-            <div className="flex justify-center items-center gap-8">
-                <div className="w-[400px] h-[400px]">
+            <div className="flex flex-col justify-center items-center gap-8">
+                <div className="w-56 h-56">
                     <img src={virus} />
                 </div>
 
-                <div>
-                    <h1 className="font-default text-7xl">404:
-                    <small>Players Not Found</small></h1>
-                    <Link to="/"><button className="bg-blue-800 rounded-full py-3 mt-10 px-8 text-white-100"> Home</button></Link>
+                <div className="w-[400px]">
+                    <h1 className="mb-10 font-default text-center"> A página que você procura não está disponível ou não existe. Volte para a página anterior</h1>
+                    <Link to="/"><a className="pt-16 text-blue-700"> Clique aqui para voltar para a página principal</a></Link>
                 </div>
                
             </div>
