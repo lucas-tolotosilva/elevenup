90ddd4a2f38f2e29efd72db4d674a940fbbf264b
joeyanovich <joedison.dias@gmail.com>
2023-01-31 11:20:01 -0300

    added image erro404

--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -1,18 +1,18 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import virus from "../../assets/virus.png"
+import virus from "../../assets/erro404.png"
 export function ErrorPAge(){
     return (
         <div className="relative overflow-hidden w-screen h-screen flex flex-col items-center justify-center text-center">
             
-            <div className="flex flex-col justify-center items-center gap-8">
-                <div className="w-56 h-56">
+            <div className="flex justify-center items-center gap-8">
+                <div className="w-96 h-96">
                     <img src={virus} />
                 </div>
 
                 <div className="w-[400px]">
                     <h1 className="mb-10 font-default text-center"> A página que você procura não está disponível ou não existe. Volte para a página anterior</h1>
-                    <Link to="/"><a className="pt-16 text-blue-700"> Clique aqui para voltar para a página principal</a></Link>
+                    <Link to="/"><a className=" text-blue-700"> Clique aqui para voltar para a página principal</a></Link>
                 </div>
                
             </div>
