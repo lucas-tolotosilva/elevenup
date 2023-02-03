6ca66037d78ba108e6f9f75b5c8701976c8aeb29
unknown <lucastoloto.silva@gmail.com>
2023-02-03 11:37:39 -0300

    Add the images

--- src/components/DataEquipe.tsx
+++ src/components/DataEquipe.tsx
@@ -1,5 +1,12 @@
 import react from 'react'
-
+import f1 from "../assets/equipe/joey.jpg"
+import f2 from "../assets/equipe/cr7.jpg"
+import f3 from "../assets/equipe/ney.jpg"
+import f4 from "../assets/equipe/lennon.jpg"
+import f5 from "../assets/equipe/bruno.jpg"
+import f6 from "../assets/equipe/maria.jpg"
+import f7 from "../assets/equipe/luis.jpg"
+import f8 from "../assets/equipe/lucas.jpg"
 
 export const listaEquipe = [
     {
