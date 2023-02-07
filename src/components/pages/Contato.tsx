ef8c45976e42a59f00a9cc1c11b3493d1bacba9c
unknown <lucastoloto.silva@gmail.com>
2023-02-07 11:59:26 -0300

    Update contato page

--- src/components/pages/Contato.tsx
+++ src/components/pages/Contato.tsx
@@ -5,10 +5,9 @@ import img from "../../assets/img-contato.jpg"
 
 export function Contato(){
     const [focus, setFocus] = useState(false)
-
-    function handleFocus () {
-
-    }
+    const [focus2, setFocus2] = useState(false)
+    const [focus3, setFocus3] = useState(false)
+    const [focus4, setFocus4] = useState(false)
 
     return (
         <div className="bg-gray-200 overflow-x-hidden">
@@ -17,36 +16,40 @@ export function Contato(){
             <Nav  />           
             {/* --------- Fim - Header (logo e menu) --------- */} 
             <div className="max-w-[1280px] bg-gray-200 absolute top-40">
-                <h1 className="font-title tracking-wider mb-24   font-bold text-[200px]">CONTATO</h1>
-                <div className="w-full flex flex-wrap overflow-hidden gap-10">
+                <h1 className="font-title tracking-wider mb-24  font-bold text-[200px]">CONTATO</h1>
+                <div className="w-full flex flex-wrap overflow-hidden">
                     <form>
-                        <div className="min-w-[1000px] flex rounded-3xl overflow-hidden bg-white-100">
-                            <div className="w-3/5 rounded-tl-3xl relative bg-pink-50">
-                                <img className="w-full h-full object-cover" src={img}/>
+                        <div className="min-w-[1000px] flex justify-center items-center rounded-3xl overflow-hidden bg-white-100">
+                            <div className="w-3/5 rounded-tl-3xl relative">
+                                <img className="w-full h-full object-contain" src={img}/>
                                 <div className="absolute flex items-center justify-center top-0 w-full h-full bg-white-80 backdrop-blur-xl">
-                                    <h2 className="font-default font-semibold text-[30px] mx-20">
+                                    <h2 className="font-default font-semibold text-[30px] text-blue-800 mx-20">
                                         Preencha o Formulário para entrar em contato conosco! <br /><br />
                                         Responderemos o mais rápido possível
                                     </h2>
                                 </div>
                                 
                             </div>
-                            <div className="w-2/5 px-14 py-14 font-default bg-white-80">
-                                <div className="relative">
-                                    <input onfocus={handleFocus()} className="relative w-full border-none bg-transparent py-2 outline-none transition duration-100" required/>
-                                    <span className="absolute p-2 left-0 pointer-events-none">Nome</span>
+                            <div className="w-2/5 px-14 py-14 font-default text-blue-800 text-[18px] bg-white-80">
+                                <div className="relative my-10">
+                                    <input onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} className="relative w-full border-none bg-transparent py-2 outline-none " required/>
+                                    <span className={`${focus ? '-translate-y-[32px] text-[16px] ' : null} transition-all duration-400 absolute py-2 left-0 pointer-events-none`}>Nome</span>
+                                    <i className="absolute bottom-0 left-0 w-full h-[2px] bg-blue-600 rounded-full"></i>
                                 </div>
-                                <div>
-                                    <input  className="relative outline-none"/>
-                                    <span>Telefone</span>
+                                <div className="relative my-10">
+                                    <input onFocus={() => setFocus2(true)} onBlur={() => setFocus2(false)} className="relative w-full border-none bg-transparent py-2 outline-none " required/>
+                                    <span className={`${focus2 ? '-translate-y-[32px] text-[16px] ' : null} transition-all duration-400 absolute py-2 left-0 pointer-events-none`}>Telefone</span>
+                                    <i className="absolute bottom-0 left-0 w-full h-[2px] bg-blue-600 rounded-full"></i>
                                 </div>
-                                <div>
-                                    <input  className="relative outline-none "/>
-                                    <span className="absolute">Email</span>
+                                <div className="relative my-10">
+                                    <input onFocus={() => setFocus3(true)} onBlur={() => setFocus3(false)} className="relative w-full border-none bg-transparent py-2 outline-none " required/>
+                                    <span className={`${focus3 ? '-translate-y-[32px] text-[16px] ' : null} transition-all duration-400 absolute py-2 left-0 pointer-events-none`}>Email</span>
+                                    <i className="absolute bottom-0 left-0 w-full h-[2px] bg-blue-600 rounded-full"></i>
                                 </div>
-                                <div>
-                                    <textarea   className="relative outline-none "/>
-                                    <span>Mensagem</span>
+                                <div className="relative my-10">
+                                    <textarea onFocus={() => setFocus4(true)} onBlur={() => setFocus4(false)} className="relative w-full border-none bg-transparent py-2 outline-none " required/>
+                                    <span className={`${focus4 ? '-translate-y-[32px] text-[16px] ' : null} transition-all duration-400 absolute py-2 left-0 pointer-events-none`}>Mensagem</span>
+                                    <i className="absolute bottom-0 left-0 w-full h-[2px] bg-blue-600 rounded-full"></i>
                                 </div>
                             </div>
                         </div>
@@ -54,7 +57,7 @@ export function Contato(){
                 </div>
             </div>
         </div>
-            <div className='flex items-center justify-center pt-96'>
+            <div className='flex items-center justify-center pt-64'>
                 <Footer />
             </div>
     </div> 
