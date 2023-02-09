d9abdbbc4de085cd7ccce9f295b8473470484dff
unknown <lucastoloto.silva@gmail.com>
2023-02-09 14:23:28 -0300

    Add the mask react

--- src/components/pages/Contato.tsx
+++ src/components/pages/Contato.tsx
@@ -9,6 +9,17 @@ export function Contato(){
     const [focus3, setFocus3] = useState(false)
     const [focus4, setFocus4] = useState(false)
 
+    const temail = 'Email'
+    const tnome = 'Nome'
+    const ttel = 'Telefone'
+
+    const [nome, setNome] = useState('')
+    const [tel, setTel] = useState('')
+    const [email, setEmail] = useState('')
+    const [msg, setMsg] = useState('')
+    
+    
+    
     return (
         <div className="bg-gray-200 overflow-x-hidden">
         <div className="relative w-screen min-h-screen flex flex-col items-center justify-center text-center mb-96 ">
@@ -23,31 +34,31 @@ export function Contato(){
                         <div className="w-[1000px] flex justify-center items-center rounded-3xl overflow-hidden bg-white-100">
                             <div className="w-1/2 rounded-tl-3xl relative">
                                 <img className="w-full h-full object-contain" src={img}/>
-                                <div className="absolute flex items-center justify-center top-0 w-full h-full bg-white-50 backdrop-blur-sm">
+                                {/* <div className="absolute flex items-center justify-center top-0 w-full h-full bg-white-50 backdrop-blur-sm">
                                     
-                                </div>
+                                </div> */}
                                 
                             </div>
-                            <div className="w-1/2 px-14 py-14 font-default text-blue-800 text-[18px] bg-white-80">
+                            <div className="w-1/2 px-14 py-14 font-default text-blue-800 text-[18px] bg-white-80">   
                                 <div className="relative my-10">
-                                    <input onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} className={`${focus ? null : 'text-transparent' } relative text-white-100 pl-2 z-20 w-full border-none bg-transparent py-2 outline-none`} required/>
-                                    <span className={`${focus ? '-translate-y-[32px] text-[16px]' : null} transition-all duration-400 absolute py-2 left-0 pointer-events-none`}>Nome</span>
-                                    <i className={`absolute bottom-0 left-0 w-full text-white-100 bg-blue-600 z-10 ${focus ? 'h-full' : 'h-[2px] '} rounded-lg transition-all ease-in-out duration-400`}></i>
+                                    <input value={nome} onChange={(e) => setNome(e.target.value)} onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} className={`${focus ? null : 'text-transparent' } relative text-black-900 pl-2 z-20 w-full border-none bg-transparent py-2 outline-none`} required/>
+                                    <span className={`${focus ? '-translate-y-[32px] text-[16px]' : null} transition-all duration-400 absolute py-2 left-0 pointer-events-none`}>{focus ? 'Nome' : nome == '' ? 'Nome' : null}</span>
+                                    <i className={`absolute bottom-0 left-0 w-full text-white-100 bg-blue-50 z-10 ${focus ? 'h-full' : 'h-[2px] '} rounded-lg transition-all ease-in-out duration-400`}></i>
                                 </div>
                                 <div className="relative my-10">
-                                    <input onFocus={() => setFocus2(true)} onBlur={() => setFocus2(false)} className={`${focus2 ? null : 'text-transparent' } relative text-white-100 pl-2 z-20 w-full border-none bg-transparent py-2 outline-none`} required/>
-                                    <span className={`${focus2 ? '-translate-y-[32px] text-[16px]' : null} transition-all duration-400 absolute py-2 left-0 pointer-events-none`}>Telefone</span>
-                                    <i className={`absolute bottom-0 left-0 w-full text-white-100 bg-blue-600 z-10 ${focus2 ? 'h-full' : 'h-[2px] '} rounded-lg transition-all ease-in-out duration-400`}></i>
+                                    <input value={tel} onChange={(e) => setTel(e.target.value)} onFocus={() => setFocus2(true)} onBlur={() => setFocus2(false)} className={`${focus2 ? null : 'text-transparent' } relative text-black-900 pl-2 z-20 w-full border-none bg-transparent py-2 outline-none`} required/>
+                                    <span className={`${focus2 ? '-translate-y-[32px] text-[16px]' : null} transition-all duration-400 absolute py-2 left-0 pointer-events-none`}>{focus2 ? 'Telefone' : tel == '' ? 'Telefone' : null}</span>
+                                    <i className={`absolute bottom-0 left-0 w-full text-white-100 bg-blue-50 z-10 ${focus2 ? 'h-full' : 'h-[2px] '} rounded-lg transition-all ease-in-out duration-400`}></i>
                                 </div>
                                 <div className="relative my-10">
-                                    <input onFocus={() => setFocus3(true)} onBlur={() => setFocus3(false)} className={`${focus3 ? null : 'text-transparent' } relative text-white-100 pl-2 z-20 w-full border-none bg-transparent py-2 outline-none`} required/>
-                                    <span className={`${focus3 ? '-translate-y-[32px] text-[16px]' : null} transition-all duration-400 absolute py-2 left-0 pointer-events-none`}>Email</span>
-                                    <i className={`absolute bottom-0 left-0 w-full text-white-100 bg-blue-600 z-10 ${focus3 ? 'h-full' : 'h-[2px] '} rounded-lg transition-all ease-in-out duration-400`}></i>
+                                    <input value={email} onChange={(e) => setEmail(e.target.value)} onFocus={() => setFocus3(true)} onBlur={() => setFocus3(false)} className={`${focus3 ? null : 'text-transparent' } relative text-black-900 pl-2 z-20 w-full border-none bg-transparent py-2 outline-none`} required/>
+                                    <span className={`${focus3 ? '-translate-y-[32px] text-[16px]' : null} transition-all duration-400 absolute py-2 left-0 pointer-events-none`}>{focus3 ? 'Email' : email == '' ? 'Email' : null}</span>
+                                    <i className={`absolute bottom-0 left-0 w-full text-white-100 bg-blue-50 z-10 ${focus3 ? 'h-full' : 'h-[2px] '} rounded-lg transition-all ease-in-out duration-400`}></i>
                                 </div>
                                 <div className="relative my-10">
-                                    <textarea onFocus={() => setFocus4(true)} onBlur={() => setFocus4(false)} className={`${focus4 ? null : 'text-transparent' } relative text-white-100 pl-2 z-20 w-full border-none bg-transparent py-2 outline-none`} required/>
-                                    <span className={`${focus4 ? '-translate-y-[32px] text-[16px]' : null} transition-all duration-400 absolute py-2 left-0 pointer-events-none`}>Mensagem</span>
-                                    <i className={`absolute bottom-0 left-0 w-full text-white-100 bg-blue-600 z-10 ${focus4 ? 'h-full' : 'h-[2px] '} rounded-lg transition-all ease-in-out duration-400`}></i>
+                                    <textarea value={msg} onChange={(e) => setMsg(e.target.value)} onFocus={() => setFocus4(true)} onBlur={() => setFocus4(false)} className={`${focus4 ? null : 'text-transparent' } relative text-black-900 pl-2 z-20 w-full border-none bg-transparent py-2 outline-none`} required/>
+                                    <span className={`${focus4 ? '-translate-y-[32px] text-[16px]' : null} transition-all duration-400 absolute py-2 left-0 pointer-events-none`}>{focus4 ? 'Mensagem' : msg == '' ? 'Mensagem' : null}</span>
+                                    <i className={`absolute bottom-0 left-0 w-full text-white-100 bg-blue-50 z-10 ${focus4 ? 'h-full' : 'h-[2px] '} rounded-lg transition-all ease-in-out duration-400`}></i>
                                 </div>
                             
                                 <button className="px-7 py-2 rounded-md bg-blue-600 text-white-100 ">Enviar</button>
