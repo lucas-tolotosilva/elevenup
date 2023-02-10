6d0d591cc606dfee297ce76bfa949f15ff8e6135
unknown <lucastoloto.silva@gmail.com>
2023-02-10 14:29:06 -0300

    Update

--- src/components/pages/QuemSomos.tsx
+++ src/components/pages/QuemSomos.tsx
@@ -1,41 +1,118 @@
 import React from "react"
 import { Footer } from "../Footer"
 import { Nav } from "../Nav"
-import img from "../../assets/quem-somos.jpg"
+import img1 from "../../assets/quem-somos.jpg"
+
 export function QuemSomos(){
     return (
-        <div className="relative bg-gray-200 w-screen h-screen flex flex-col items-center text-center">
-            {/* --------- Header (logo e menu) --------- */}
-            <Nav  />           
+    <div className="bg-gray-200 overflow-x-hidden">
+            <div className="relative w-screen min-h-screen flex flex-col items-center justify-center text-center mb-56 ">
+            <Nav />
             
-            {/* --------- Fim - Header (logo e menu) --------- */} 
-<div className="text-9xl">
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-<h2>Olá</h2>
-</div>
+            <div className="max-w-[1280px] bg-gray-200 absolute top-40">
+                <h1 className="font-title tracking-wider mb-24   font-bold text-[150px]">quem somos</h1>
+                <div className="flex justify-end  items-center">
+                    <img className="w-1/2 cover rounded-3xl" src={img1} />
+                    <div className="text-justify pl-44 py-10 rounded-full -ml-52 bg-gray-200 ">
+                        <p>
+                        Nulla sodales orci sit amet pellentesque pretium. 
+                        Quisque elementum blandit faucibus. Aenean vitae 
+                        quam sollicitudin, vehicula velit at, imperdiet 
+                        turpis. Sed sed enim id velit posuere mollis. 
+                        Suspendisse eget pharetra nunc. Fusce eu tellus
+                        et ante tempor rutrum. In vehicula nisl eu sapien 
+                        tincidunt facilisis. Donec vel metus et magna laoreet
+                        pulvinar ac ac ipsum. Aenean molestie sem at volutpat 
+                        maximus. Nunc laoreet, nisl et euismod blandit, augue 
+                        neque dictum ante, quis hendrerit urna turpis ac purus. 
+                        Integer pharetra nibh nec erat faucibus, a pulvinar magna 
+                        placerat.
+                        <br /> <br />
+                        Proin vitae arcu enim. Donec eu tellus at augue tempus 
+                        ultrices. Aliquam malesuada vehicula accumsan. In hac 
+                        habitasse platea dictumst. Nam eget consequat nunc, non 
+                        varius est. Quisque velit mi, fermentum sit amet mauris 
+                        vel, faucibus finibus tellus. Donec et felis eleifend, 
+                        faucibus quam quis, porttitor augue. Sed sit amet mauris 
+                        turpis. Suspendisse nec imperdiet sapien. In mollis eros 
+                        eu sem egestas lacinia. Nullam vitae ligula at leo 
+                        malesuada posuere sit amet quis massa. Pellentesque 
+                        mattis interdum porttitor. Duis nec tincidunt diam, 
+                        sed finibus eros. Aenean auctor ipsum quis lacus porta 
+                        efficitur. Suspendisse euismod dictum libero et viverra.
+                        </p>
+                    </div>
+                </div>
 
-            
-        </div> 
+            </div>   
+            </div>
+
+            <div className="w-screen bg-black-900 h-64 text-white-100 font-default text-[40px] flex justify-center items-center mb-56">
+                <span>Nulla sodales orci sit amet pellentesque pretium</span>
+            </div>
+
+            <div className=" w-screen flex items-center justify-center">
+            <div className="max-w-[1280px] text-justify ">
+                        <p>
+                        Nulla sodales orci sit amet pellentesque pretium. 
+                        Quisque elementum blandit faucibus. Aenean vitae 
+                        quam sollicitudin, vehicula velit at, imperdiet 
+                        turpis. Sed sed enim id velit posuere mollis. 
+                        Suspendisse eget pharetra nunc. Fusce eu tellus
+                        et ante tempor rutrum. In vehicula nisl eu sapien 
+                        tincidunt facilisis. Donec vel metus et magna laoreet
+                        pulvinar ac ac ipsum. Aenean molestie sem at volutpat 
+                        maximus. Nunc laoreet, nisl et euismod blandit, augue 
+                        neque dictum ante, quis hendrerit urna turpis ac purus. 
+                        Integer pharetra nibh nec erat faucibus, a pulvinar magna 
+                        placerat.
+                        <br /> <br />
+                        Proin vitae arcu enim. Donec eu tellus at augue tempus 
+                        ultrices. Aliquam malesuada vehicula accumsan. In hac 
+                        habitasse platea dictumst. Nam eget consequat nunc, non 
+                        varius est. Quisque velit mi, fermentum sit amet mauris 
+                        vel, faucibus finibus tellus. Donec et felis eleifend, 
+                        faucibus quam quis, porttitor augue. Sed sit amet mauris 
+                        turpis. Suspendisse nec imperdiet sapien. In mollis eros 
+                        eu sem egestas lacinia. Nullam vitae ligula at leo 
+                        malesuada posuere sit amet quis massa. Pellentesque 
+                        mattis interdum porttitor. Duis nec tincidunt diam, 
+                        sed finibus eros. Aenean auctor ipsum quis lacus porta 
+                        efficitur. Suspendisse euismod dictum libero et viverra.
+                        </p>
+                        <p>
+                        Nulla sodales orci sit amet pellentesque pretium. 
+                        Quisque elementum blandit faucibus. Aenean vitae 
+                        quam sollicitudin, vehicula velit at, imperdiet 
+                        turpis. Sed sed enim id velit posuere mollis. 
+                        Suspendisse eget pharetra nunc. Fusce eu tellus
+                        et ante tempor rutrum. In vehicula nisl eu sapien 
+                        tincidunt facilisis. Donec vel metus et magna laoreet
+                        pulvinar ac ac ipsum. Aenean molestie sem at volutpat 
+                        maximus. Nunc laoreet, nisl et euismod blandit, augue 
+                        neque dictum ante, quis hendrerit urna turpis ac purus. 
+                        Integer pharetra nibh nec erat faucibus, a pulvinar magna 
+                        placerat.
+                        <br /> <br />
+                        Proin vitae arcu enim. Donec eu tellus at augue tempus 
+                        ultrices. Aliquam malesuada vehicula accumsan. In hac 
+                        habitasse platea dictumst. Nam eget consequat nunc, non 
+                        varius est. Quisque velit mi, fermentum sit amet mauris 
+                        vel, faucibus finibus tellus. Donec et felis eleifend, 
+                        faucibus quam quis, porttitor augue. Sed sit amet mauris 
+                        turpis. Suspendisse nec imperdiet sapien. In mollis eros 
+                        eu sem egestas lacinia. Nullam vitae ligula at leo 
+                        malesuada posuere sit amet quis massa. Pellentesque 
+                        mattis interdum porttitor. Duis nec tincidunt diam, 
+                        sed finibus eros. Aenean auctor ipsum quis lacus porta 
+                        efficitur. Suspendisse euismod dictum libero et viverra.
+                        </p>
+                    </div>
+            </div>
+
+            <div className='flex items-center justify-center pt-64'>
+                <Footer />
+            </div>
+    </div> 
     )
 }
