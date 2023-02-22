cd9a673f707f212826eef309932c139a6c91b328
unknown <lucastoloto.silva@gmail.com>
2023-02-22 16:31:38 -0300

    Update

--- src/components/Clientes.tsx
+++ src/components/Clientes.tsx
@@ -7,92 +7,69 @@ export function Clientes () {
     return (
         <div className="w-full mb-36">
             <motion.h2 
-                className="font-title w-full lg:text-[150px] sm:text-[50px] mb-24"
+                className="font-title w-full lg:text-[150px] sm:text-[50px] sm:mt-10 sm:mb-10 mb-24"
                 initial={{ opacity: 0 , scale: 0}}
                 whileInView={{ opacity: 1 , scale: 1}}
                 viewport={{once:true}}> Clientes </motion.h2>
                       
+            <div className='sm:hidden lg:block'>
+                <ParallaxProvider >
+                    <Parallax speed={10} translateY={[0 ,0]} translateX={[-30, 5]}>
+    
+                        <motion.div 
+                            className="w-full mb-4"
+                            initial={{ opacity:0 , scale: 0}}
+                            whileInView={{ opacity: 1 , scale: 1}}
+                            viewport={{once:true}}> 
+
+                                <div id="client-logo" className="flex gap-12 ">
+                                    {carousel1.map((item, indice)=> (
+                                        <img id="img" key={item.id} className="grayscale hover:grayscale-0 hover:scale-125  hover:cursor-pointer" src={item.img} alt='imagem' />
+                                    ))}
+                                    {carousel1.map((item, indice)=> (
+                                        <img id="img" key={item.id} className="grayscale hover:grayscale-0 hover:scale-125  hover:cursor-pointer" src={item.img} alt='imagem' />
+                                    ))}
+                                </div>
+                            
+                        </motion.div>
+                </Parallax>
+
+                <Parallax translateX={[5, -30]}>
+                    <motion.div 
+                        className="w-full"
+                        initial={{ opacity:0 , scale: 0}}
+                        whileInView={{ opacity: 1 , scale: 1}}
+                        viewport={{once:true}}> 
+                        
+                            <div id="client-logo" className="flex gap-12">
+                                {carousel2.map((item, indice)=> (
+                                    <img key={item.id} className="cover hover:grayscale-0 hover:cursor-pointer hover:scale-125 grayscale" src={item.img} alt='imagem' />
+                                ))}
+                                {carousel2.map((item, indice)=> (
+                                    <img key={item.id} className="cover hover:grayscale-0 hover:cursor-pointer hover:scale-125 grayscale" src={item.img} alt='imagem' />
+                                ))}
+                            </div>
+                    </motion.div>
+
+                </Parallax>
+
             
-            <ParallaxProvider >
-                <Parallax speed={10} translateY={[0 ,0]} translateX={[-30, 5]}>
- 
-                    <motion.div 
-                        className="w-full mb-4"
-                        initial={{ opacity:0 , scale: 0}}
-                        whileInView={{ opacity: 1 , scale: 1}}
-                        viewport={{once:true}}> 
+            </ParallaxProvider>
+            </div>
 
-                            <div id="client-logo" className="flex gap-12 ">
-                                {carousel1.map((item, indice)=> (
-                                    <img id="img" key={item.id} className="grayscale hover:grayscale-0 hover:scale-125  hover:cursor-pointer" src={item.img} alt='imagem' />
-                                ))}
-                                {carousel1.map((item, indice)=> (
-                                    <img id="img" key={item.id} className="grayscale hover:grayscale-0 hover:scale-125  hover:cursor-pointer" src={item.img} alt='imagem' />
-                                ))}
-                            </div>
+            <span className='font-default text-[15px] text-center'>Arraste para o lado para ver todos os clientes!</span>
+            <div className='sm:ml-4 sm:block lg:hidden'>
+                <div id="client-logo" className="flex gap-12 overflow-auto overflow-y-hidden touch-auto">
+                    {carousel1.map((item, indice)=> (
+                        <img id="img" key={item.id} className="grayscale hover:grayscale-0 hover:scale-125  hover:cursor-pointer" src={item.img} alt='imagem' />
+                    ))}
+                     {carousel2.map((item, indice)=> (
+                        <img key={item.id} className="cover hover:grayscale-0 hover:cursor-pointer hover:scale-125 grayscale" src={item.img} alt='imagem' />
+                    ))}
+                </div>
+
+            </div>
                         
-                    </motion.div>
-            </Parallax>
-
-            <Parallax translateX={[5, -30]}>
-                <motion.div 
-                    className="w-full"
-                    initial={{ opacity:0 , scale: 0}}
-                    whileInView={{ opacity: 1 , scale: 1}}
-                    viewport={{once:true}}> 
-                    
-                        <div id="client-logo" className="flex gap-12">
-                            {carousel2.map((item, indice)=> (
-                                <img key={item.id} className="cover hover:grayscale-0 hover:cursor-pointer hover:scale-125 grayscale" src={item.img} alt='imagem' />
-                            ))}
-                            {carousel2.map((item, indice)=> (
-                                <img key={item.id} className="cover hover:grayscale-0 hover:cursor-pointer hover:scale-125 grayscale" src={item.img} alt='imagem' />
-                            ))}
-                        </div>
-                </motion.div>
-
-            </Parallax>
-
-            <Parallax speed={10} translateY={[0 ,0]} translateX={[-120, -80]}>
- 
-                    <motion.div 
-                        className="lg:hidden w-full mb-4"
-                        initial={{ opacity:0 , scale: 0}}
-                        whileInView={{ opacity: 1 , scale: 1}}
-                        viewport={{once:true}}> 
-
-                            <div id="client-logo" className="flex gap-12 ">
-                                {carousel1.map((item, indice)=> (
-                                    <img id="img" key={item.id} className="grayscale hover:grayscale-0 hover:scale-125  hover:cursor-pointer" src={item.img} alt='imagem' />
-                                ))}
-                                {carousel1.map((item, indice)=> (
-                                    <img id="img" key={item.id} className="grayscale hover:grayscale-0 hover:scale-125  hover:cursor-pointer" src={item.img} alt='imagem' />
-                                ))}
-                            </div>
-                        
-                    </motion.div>
-            </Parallax>
-
-            <Parallax translateY={[0 ,0]} translateX={[-80, -120]}>
-                <motion.div 
-                    className="lg:hidden w-full"
-                    initial={{ opacity:0 , scale: 0}}
-                    whileInView={{ opacity: 1 , scale: 1}}
-                    viewport={{once:true}}> 
-                    
-                        <div id="client-logo" className="flex gap-12">
-                            {carousel2.map((item, indice)=> (
-                                <img key={item.id} className="cover hover:grayscale-0 hover:cursor-pointer hover:scale-125 grayscale" src={item.img} alt='imagem' />
-                            ))}
-                            {carousel2.map((item, indice)=> (
-                                <img key={item.id} className="cover hover:grayscale-0 hover:cursor-pointer hover:scale-125 grayscale" src={item.img} alt='imagem' />
-                            ))}
-                        </div>
-                </motion.div>
-
-            </Parallax>
-
-        </ParallaxProvider>
         </div>
     )
 }
