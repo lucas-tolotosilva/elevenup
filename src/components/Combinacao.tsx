ec2dceec5c04ac85fe439cae2103fd2a2f8ae9d6
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2023-02-24 16:16:14 -0300

    Update

--- src/components/Combinacao.tsx
+++ src/components/Combinacao.tsx
@@ -5,92 +5,92 @@ import health from '../assets/health.jpg'
 
 export function Combinacao() {
     return (
-        <div className='w-full mb-36'>
-            <motion.h2 
+        <div className='w-full relative mb-96'>
+            <motion.h2
                 className="font-title w-full sm:text-[45px] sm:text-center lg:text-[150px] mb-4"
-                initial={{opacity:0, scale: 0.7}}
-                whileInView={{ opacity: 1, scale: 1}}
-                viewport={{once:true}}>A Combinação Perfeita</motion.h2>
+                initial={{ opacity: 0, scale: 0.7 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ once: true }}>A Combinação Perfeita</motion.h2>
 
-            <motion.p 
+            <motion.p
                 className='font-default sm:text-[14px] lg:text[18px] sm:mx-5'
-                initial={{opacity:0, scale: 0.7}}
-                whileInView={{ opacity: 1, scale: 1}}
-                viewport={{once:true}}>
-                    Ao se unir a ElevenUp você estará junto a profissionais que entendem do seu negócio e utilizam as melhores <br />
-                    ferramentaspara desenvolver sua empresa, fazer você se relacionar com seu cliente e vender mais!
+                initial={{ opacity: 0, scale: 0.7 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                viewport={{ once: true }}>
+                Ao se unir a ElevenUp você estará junto a profissionais que entendem do seu negócio e utilizam as melhores <br />
+                ferramentaspara desenvolver sua empresa, fazer você se relacionar com seu cliente e vender mais!
             </motion.p>
 
-            <motion.h3 
-                className='font-title lg:text-7xl sm:text-[30px] sm:my-16 lg:my-20'
-                initial={{opacity:0,y:100, scale: 0.7}}
-                whileInView={{ opacity: 1,y:0, scale: 1}}
-                viewport={{once:true}}> Qual o segmento da sua empresa?
+            <motion.h3
+                className='font-title lg:text-7xl sm:text-[30px] lg:mb-36 sm:my-16 lg:my-20'
+                initial={{ opacity: 0, y: 100, scale: 0.7 }}
+                whileInView={{ opacity: 1, y: 0, scale: 1 }}
+                viewport={{ once: true }}> Qual o segmento da sua empresa?
             </motion.h3>
 
             <div className='w-full  flex justify-center'>
                 <div className='lg:max-w-[1280px] sm:text-[14px] lg:min-w-[1270px] sm:w-full sm:flex-wrap flex justify-center items-center'>
-                    <motion.div 
-                        className='lg:w-6/12 sm:w-full sm:mx-5 z-10 lg:mr-14 sm:my-5 p-12 text-left rounded-3xl bg-white-150'
-                        initial={{opacity:0, scale: 0}}
-                        whileInView={{ opacity: 1, scale: 1}}
-                        viewport={{once:true}}
-                        transition={{type:"spring", delay: 0.3, bounce: 0.5}}>
-                            <div>
-                                <h4 className='font-title text-left mb-6 text-4xl'>Life Science</h4>
-                                <p className='font-default w-11/12'>  
-                                    – Representantes de materiais laboratoriais e
-                                    médico-hospitalares. <br />
-                                    – Prestadores de serviços de suporte e manutenção. <br />
-                                    – Softwares relacionados à Biotech. <br />
-                                    – Indústria de materiais laboratoriais
-                                    médico-hospitalares. <br />
-                                </p>
-                            </div>
+                    <motion.div
+                        className='lg:w-6/12 sm:w-full sm:mx-5 z-10 lg:mr-14 sm:my-5 p-12 text-left rounded-3xl bg-white-150 shadow-3xl'
+                        initial={{ opacity: 0, scale: 0 }}
+                        whileInView={{ opacity: 1, scale: 1 }}
+                        viewport={{ once: true }}
+                        transition={{ type: "spring", delay: 0.3, bounce: 0.5 }}>
+                        <div>
+                            <h4 className='font-title text-left mb-6 text-4xl'>Life Science</h4>
+                            <p className='font-default w-11/12'>
+                                – Representantes de materiais laboratoriais e
+                                médico-hospitalares. <br />
+                                – Prestadores de serviços de suporte e manutenção. <br />
+                                – Softwares relacionados à Biotech. <br />
+                                – Indústria de materiais laboratoriais
+                                médico-hospitalares. <br />
+                            </p>
+                        </div>
                     </motion.div>
 
-                    <motion.div 
-                        className='lg:w-6/12 sm:w-full sm:mx-5 z-10 lg:mr-14 sm:my-5 p-12 text-left rounded-3xl bg-white-150'
-                        initial={{opacity:0, scale: 0}}
-                        whileInView={{ opacity: 1, scale: 1}}
-                        transition={{type:"spring", delay: 0.3, bounce: 0.5}}
-                        viewport={{once:true}}>
-                            <div>
-                                <h4 className='font-title text-left mb-6 text-4xl'>Healthcare</h4>
-                                <p className='font-default w-11/12'>  
-                                    – Laboratórios de análise. <br />
-                                    – Serviços especializados em saúde.<br />
-                                    – Empresas de equipamentos especializados.<br />
-                                    – Empresas de plano de saúde (clínicas populares).<br />
-                                    – Clínicas de saúde.<br /><br />
-                                </p>
-                            </div>
+                    <motion.div
+                        className='lg:w-6/12 sm:w-full sm:mx-5 z-10 lg:mr-14 sm:my-5 p-12 text-left rounded-3xl bg-white-150 shadow-3xl'
+                        initial={{ opacity: 0, scale: 0 }}
+                        whileInView={{ opacity: 1, scale: 1 }}
+                        transition={{ type: "spring", delay: 0.3, bounce: 0.5 }}
+                        viewport={{ once: true }}>
+                        <div>
+                            <h4 className='font-title text-left mb-6 text-4xl'>Healthcare</h4>
+                            <p className='font-default w-11/12'>
+                                – Laboratórios de análise. <br />
+                                – Serviços especializados em saúde.<br />
+                                – Empresas de equipamentos especializados.<br />
+                                – Empresas de plano de saúde (clínicas populares).<br />
+                                – Clínicas de saúde.<br /><br />
+                            </p>
+                        </div>
                     </motion.div>
 
                 </div>
             </div>
 
-            <div className='w-full z-10 sm:hidden lg:flex justify-center'>
+            <div className='w-full sm:hidden lg:flex justify-center lg:absolute lg:bottom-0 z-0 '>
                 <div className='max-w-[1280px] min-w-[1270px] flex items-center justify-center'>
-                    <motion.div 
-                        className='w-6/12 -mt-40 ml-16 mr-32'
-                        initial={{opacity:0,y: 100, scale: 0}}
-                        whileInView={{ opacity: 1,y:0, scale: 1}}
-                        transition={{delay: 0.5}}
-                        viewport={{once:true}}>
-                            <img className='w-full rounded-3xl' src={life} />
+                    <motion.div
+                        className='w-6/12 blur-sm -mt-40 ml-16 mr-32'
+                        initial={{ opacity: 0, y: 100, scale: 0 }}
+                        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+                        transition={{ delay: 0.5 }}
+                        viewport={{ once: true }}>
+                        <img className='w-full rounded-3xl' src={life} />
                     </motion.div>
-                    <motion.div 
-                        className='w-6/12 -mt-40 ml-32 mr-16'
-                        initial={{opacity:0,y: 100, scale: 0}}
-                        whileInView={{ opacity: 1,y:0, scale: 1}}
-                        transition={{delay: 0.5}}
-                        viewport={{once:true}}>
-                            <img className='w-full rounded-3xl' src={health} />
+                    <motion.div
+                        className='w-6/12 blur-sm -mt-40 ml-32 mr-16'
+                        initial={{ opacity: 0, y: 100, scale: 0 }}
+                        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+                        transition={{ delay: 0.5 }}
+                        viewport={{ once: true }}>
+                        <img className='w-full rounded-3xl' src={health} />
                     </motion.div>
                 </div>
             </div >
-            
+
         </div>
     )
 }
--- src/components/pages/Cases.tsx
+++ src/components/pages/Cases.tsx
@@ -6,21 +6,26 @@ import img from "../../assets/logo.png"
 export function Cases() {
     return (
         <div className="bg-gray-200 overflow-x-hidden">
-            <div className="relative w-screen h-screen flex flex-col items-center justify-center text-center mb-96 ">
+            <div className="relative w-screen min-h-screen flex flex-col items-center justify-center text-center">
                 {/* --------- Header (logo e menu) --------- */}
                 <Nav />
                 {/* --------- Fim - Header (logo e menu) --------- */}
-                <div className="max-w-[1280px] mb-96 pb-96 bg-gray-200 absolute top-40">
-                    <h1 className="font-title tracking-wider font-bold text-[150px]">CASES</h1>
-                    <div className="w-full z-50 overflow-hidden gap-10">
+                <div className="max-w-[1280px] mb-10  bg-gray-200 mt-40">
+                    <h1 className="font-title tracking-wider font-bold sm:text-[80px] sm:mb-10 lg:text-[150px]">CASES</h1>
+                    <div className="w-full z-50 overflow-hidden ">
                         <Case link={casesList[0].link} img={casesList[0].img} content={casesList[0].content} />
                         <Case link={casesList[1].link} img={casesList[1].img} content={casesList[1].content} />
                         <Case link={casesList[2].link} img={casesList[2].img} content={casesList[2].content} />
                     </div>
                 </div>
             </div>
-            <div className='flex items-center pt-96 justify-center'>
+            {/* DESKTOP */}
+            <div className='lg:flex sm:hidden lg:items-center lg:justify-center pt-10'>
                 <Footer />
+            </div>
+            {/* MOBILE */}
+            <div className='sm:inline lg:hidden text-center '>
+                <FooterMobile />
             </div>
         </div>
     )
@@ -37,13 +42,13 @@ export function Case({ img, content, link }: casesProps) {
     }
 
     return (
-        <div className="flex min-w-[1000px]  my-10 h-72">
-            <div className="w-1/4 rounded-tl-3xl flex justify-center rounded-bl-3xl bg-white-90 ">
-                <img className='object-contain w-[280px] h-[280px] p-5' src={img} />
+        <div className="sm:flex sm:flex-col lg:flex-row sm:items-center sm:justify-center lg:flex sm:w-full lg:min-w-[1000px]  sm:mb-56 my-10 h-72">
+            <div className="sm:w-1/2 lg:w-1/4 rounded-tl-3xl flex justify-center rounded-bl-3xl lg:bg-white-90 ">
+                <img className='object-contain lg:w-[280px] sm:w-[auto] sm:h-auto lg:h-[280px] p-5' src={img} />
             </div>
-            <div className="font-default text-[20px] text-justify flex flex-col justify-center items-center mx-4 px-10 w-3/4 rounded-tr-3xl rounded-br-3xl bg-white-80 ">
+            <div className="font-default  lg:text-[20px] sm:text-[14px] text-justify flex flex-col justify-center items-center sm:pt-10 mx-4 px-10 w-3/4 lg:rounded-tr-3xl lg:rounded-br-3xl bg-white-80 ">
                 {truncate(content, 255)}
-                <a href={link}><button className=" rounded-full bg-blue-900 text-white-100 text-[18px] px-10 py-1 mt-10 hover:cursor-pointer">Saiba Mais!</button></a>
+                <a href={link}><button className=" rounded-full bg-blue-900 text-white-100 lg:text-[20px] sm:text-[14px] px-10 py-1 my-10 hover:cursor-pointer">Saiba Mais!</button></a>
             </div>
         </div>
     )
