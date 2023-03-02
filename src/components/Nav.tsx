616116058715b25c604524e957777fd5f8e1f88a
unknown <lucastoloto.silva@gmail.com>
2023-03-02 15:04:56 -0300

    Update

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-//import { Link } from "react-router-dom"
-import { HashLink as Link } from 'react-router-hash-link';
+import { Link } from "react-router-dom"
 import '../styles/main.css'
 import { motion, } from "framer-motion"
 import logo from '../assets/logo.png'
