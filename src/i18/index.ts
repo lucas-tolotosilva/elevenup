aeea10213127f187bce13c5cbc8d4fb60f14c4f1
unknown <lucastoloto.silva@gmail.com>
2023-05-16 14:57:42 -0300

    Translate setup

---
+++ src/i18/index.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import pt from './locales/pt/pt-br.json'
+import en from './locales/en/en-us.json'
+// Adicione as traduções que você deseja suportar
+const resources = {
+  en: { ...en },
+  pt: { ...pt }
+
+};
+
+i18n
+  .use(initReactI18next) // Adiciona o plugin do React para o i18next
+  .init({
+    resources,
+    lng: 'pt', // Define o idioma padrão como português
+    interpolation: {
+      escapeValue: false // Permite que você insira valores HTML nas traduções
+    }
+  });
+
+export default i18n;
